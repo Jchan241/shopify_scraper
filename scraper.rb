96c51823ec60f3a4a3ed8201134240eb3aa8063f
jc <johchan24@gmail.com>
2020-06-19 23:22:11 +1000

    Successfully pulled data from shopify

---
+++ scraper.rb
@@ -0,0 +1,31 @@
+require 'json'
+require 'open-uri'
+require 'csv'
+
+url = 'https://kflsupermarket.com.au/products.json?limit=250&page=2'
+
+products_serialized = open(url).read
+products = JSON.parse(products_serialized)['products']
+
+# puts products.first['title']
+
+# products.each do |product|
+#   puts product['title']
+#   puts product['product_type']
+#   product['tags'].each do |tag|
+#     puts tag
+#   end
+#   product['variants'].each do |variant|
+#     puts variant['price']
+#   end
+# end
+
+CSV.open("KFLproducts.csv", "w") do |csv|
+  products.each do |product|
+    price = ''
+    product['variants'].each do |variant|
+      price = variant['price']
+    end
+    csv << [product['title'], product['tags'], product['product_type'], price]
+  end
+end
